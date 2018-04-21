dfcc0612f03a1436f38dae9c3bc7e92819ecc998
fastvepik <andrey.yurin55@gmail.com>
2018-04-21 09:25:23 +0300

    change smth

--- core/src/com/ltc/entities/WallEntity.java
+++ core/src/com/ltc/entities/WallEntity.java
@@ -60,7 +60,7 @@ public class WallEntity extends Actor {
 
         // Give it some shape.
         PolygonShape box = new PolygonShape();      // (1) Create the shape.
-        box.setAsBox(size_x, size_y);                   // (2) 1x1 meter box.
+        box.setAsBox(size_x/150, size_y/150);                   // (2) 1x1 meter box.
         fixture = body.createFixture(box, 1000000000);       // (3) Create the fixture.
         fixture.setUserData("wall");              // (4) Set the user data.
         box.dispose();                              // (5) Destroy the shape.
