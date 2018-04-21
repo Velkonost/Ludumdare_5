fe38f91982ef075daffc7e9c7bc17d70c4c9bdf3
fastvepik <andrey.yurin55@gmail.com>
2018-04-21 13:27:48 +0300

    added wall

--- core/src/com/ltc/screens/GameScreen.java
+++ core/src/com/ltc/screens/GameScreen.java
@@ -123,6 +123,8 @@ public class GameScreen extends BaseScreen {
 
       //Horizontal
         walls.add(factory.createWall(world, new Vector2(6.1f, 2.2f), 200f, 10f, -100f, 0f, "wall2", 1.15f, 0.1f));
+        walls.add(factory.createWall(world, new Vector2(14.9f, 2.2f), 200f, 10f, -100f, 0f, "wall2", 1.15f, 0.1f));
+        walls.add(factory.createWall(world, new Vector2(17.1f, 2.2f), 200f, 10f, -100f, 0f, "wall2", 1.15f, 0.1f));
         walls.add(factory.createWall(world, new Vector2(6.1f, 12.2f), 200f, 10f, -100f, 0f, "wall2", 1.15f, 0.1f));
         walls.add(factory.createWall(world, new Vector2(8.3f, 12.2f), 200f, 10f, -100f, 0f, "wall2", 1.15f, 0.1f));
         walls.add(factory.createWall(world, new Vector2(8.3f, 18.2f), 200f, 10f, -100f, 0f, "wall2", 1.15f, 0.1f));
